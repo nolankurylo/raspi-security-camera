1e7f9105f624a7d8517bac0bbf66436c8647f188
Nolan <kurylon@telus.net>
2019-06-27 22:47:09 -0700

    added video functionality

--- routes/get.js
+++ routes/get.js
@@ -3,19 +3,14 @@ var router = express.Router();
 var db = require('../queries').getDb()
 
 router.get('/', function(req, res, next) {
-    var x = db.collection("images").findOne({}, function (err, result) {
-      if(err) console.log(err)
-      console.log(result)
-    })
-   
   res.render('index', { title: `Home` });
 });
 
 router.get('/library', function (req, res, next) {
-  var x = db.collection("images").find().toArray( function (err, result) {
+  var x = db.collection("videos").find().toArray( function (err, result) {
     if (err) console.log(err)
     console.log(result)
-    return res.render("library", { title: `Library`, images: result });
+    return res.render("library", { title: `Library`, videos: result });
   })
 
 });
--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,7 @@ var multer = require("multer")
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb){
-    cb(null, "public/images/")
+    cb(null, "public/videos/")
   },
   filename: function(req, file, cb){  
     var date = new Date().toISOString()  
@@ -17,7 +17,9 @@ const storage = multer.diskStorage({
 var upload = multer({storage:storage});
 
 router.post('/insert', upload.single('media'), function(req, res) {
-  var x = db.collection("images").insertOne({filename: req.file.filename, path: req.file.path}, function(err, result) {
+  console.log(req.file)
+  console.log("insert")
+  var x = db.collection("videos").insertOne({filename: req.file.filename, path: req.file.path}, function(err, result) {
     if (err) console.log(err);
     res.status(200).send({ msg: 'There has been a success i think!' });
   });

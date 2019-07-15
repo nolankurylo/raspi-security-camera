10253f999726557010d7a9c8922ff82cfac51d4d
Nolan <kurylon@telus.net>
2019-07-14 22:05:11 -0700

    added file system to remove local videos

--- router.js
+++ router.js
@@ -2,7 +2,8 @@ var express = require("express");
 var router = express.Router();
 var db = require("./queries").getDb();
 var multer = require("multer")
-const ObjectID = require('mongodb').ObjectID;
+var ObjectID = require('mongodb').ObjectID;
+var fs = require('fs')
 
 // Middlewere for converting videos sent from the pi (.h264 -> .mp4)
 const storage = multer.diskStorage({
@@ -20,7 +21,7 @@ var upload = multer({storage:storage});
 
 // Index page (render)
 router.get('/', function (req, res, next) {
-  var x = db.collection("videos").find().toArray(function (err, result) {
+  db.collection("videos").find().toArray(function (err, result) {
     if (err) console.log(err)
     return res.render("index", {videos: result});
   })
@@ -30,7 +31,7 @@ router.get('/', function (req, res, next) {
 // Endpoint for raspi to send video to 
 router.post('/insert', upload.single('media'), function(req, res) {
   // twilio goes here
-  var x = db.collection("videos").insertOne({filename: req.file.filename, path: req.file.path, name: req.file.filename, timestamp: new Date()}, function(err, result) {
+  db.collection("videos").insertOne({filename: req.file.filename, path: req.file.path, name: req.file.filename, timestamp: new Date()}, function(err, result) {
     if (err) console.log(err);
     res.status(200).send({});
   });
@@ -38,7 +39,7 @@ router.post('/insert', upload.single('media'), function(req, res) {
 
 // Endpoint to update name of a video
 router.patch('/edit_name', function (req, res) {
-    var x = db.collection("videos").updateOne({"_id": new ObjectID(req.body.id)},{ $set: {"name": req.body.name}}, {upsert: false}, function (err, result) {
+    db.collection("videos").updateOne({"_id": new ObjectID(req.body.id)},{ $set: {"name": req.body.name}}, {upsert: false}, function (err, result) {
         if (err) console.log(err);
         res.status(200).send({});
     });
@@ -47,11 +48,20 @@ router.patch('/edit_name', function (req, res) {
 
 // Endpoint to delete a video
 router.post('/delete_video', function (req, res) {
-    var x = db.collection("videos").deleteOne({"_id": new ObjectID(req.body.id)}, function (err, result) {
-        if (err) console.log(err);
-        res.status(200).send({});
+  // Get the path name for the video to delete it from the directory
+  db.collection("videos").findOne({"_id": new ObjectID(req.body.id)}, function (err, result) {
+    if (err) console.log(err);
+    path = result.path
+    // Delete the video file from the directory
+    fs.unlink(path, function (err) {            
+        if (err) console.log(err);                         
+      // Delete the video file from database too
+      db.collection("videos").deleteOne({"_id": new ObjectID(req.body.id)}, function (err, result) {
+          if (err) console.log(err);
+          res.status(200).send({});
+      });
     });
-
+  });
 });
 
 module.exports = router;

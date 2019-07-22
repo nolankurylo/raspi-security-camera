c2573d9893ee9d45d4514769b3dcfb1bcab831c4
Nolan <kurylon@telus.net>
2019-07-21 18:36:46 -0700

    Twilio implementation and commented code

--- router.js
+++ router.js
@@ -5,13 +5,19 @@ var multer = require("multer")
 var ObjectID = require('mongodb').ObjectID;
 var fs = require('fs')
 
-// Middlewere for converting videos sent from the pi (.h264 -> .mp4)
+// Twilio configuration
+const accountSid = "AC99e2af84dce6a9e8c8b7f7d39e0cdca4";
+const authToken = "7c8a058357f7638f7dee531f08dcc98a";
+const client = require("twilio")(accountSid, authToken);
+
+// Middlewere to convert videos sent from the pi (.h264 -> .mp4)
 const storage = multer.diskStorage({
   destination: function(req, file, cb){
     cb(null, "public/videos/")
   },
   filename: function(req, file, cb){  
-    var date = new Date().toISOString()  
+    var date = new Date().toISOString()
+    // Create a filename that can be parsed by Pug
     date = date.replace(/\/|:|\./g, "-");
     cb(null, date + file.originalname )
   }
@@ -25,15 +31,26 @@ router.get('/', function (req, res, next) {
     if (err) console.log(err)
     return res.render("index", {videos: result});
   })
-
 });
 
 // Endpoint for raspi to send video to 
 router.post('/insert', upload.single('media'), function(req, res) {
-  // twilio goes here
-  db.collection("videos").insertOne({filename: req.file.filename, path: req.file.path, name: req.file.filename, timestamp: new Date()}, function(err, result) {
-    if (err) console.log(err);
-    res.status(200).send({});
+  // Send SMS Twilio message to my phone
+  client.messages
+    .create({
+      body: `The Raspberry Pi Security Camera detected movement just now at: ${new Date().toLocaleString()}`,
+      from: '+12049008834',
+      to: '+14038092980'
+    })
+    .then(function (message) {    
+      db.collection("videos").insertOne({filename: req.file.filename, path: req.file.path, name: req.file.filename, timestamp: new Date()}, function(err, result) {
+        if (err) console.log(err);
+        res.status(200).send({});
+      });
+  })
+  .catch(function(err) {
+     console.log(err)
+     res.status(500).send(err);
   });
 });
 
@@ -48,14 +65,14 @@ router.patch('/edit_name', function (req, res) {
 
 // Endpoint to delete a video
 router.post('/delete_video', function (req, res) {
-  // Get the path name for the video to delete it from the directory
+  // Get the path name for the video to delete it from local directory
   db.collection("videos").findOne({"_id": new ObjectID(req.body.id)}, function (err, result) {
     if (err) console.log(err);
     path = result.path
-    // Delete the video file from the directory
+    // Delete the video file from local directory
     fs.unlink(path, function (err) {            
         if (err) console.log(err);                         
-      // Delete the video file from database too
+      // Delete the video file from database 
       db.collection("videos").deleteOne({"_id": new ObjectID(req.body.id)}, function (err, result) {
           if (err) console.log(err);
           res.status(200).send({});
